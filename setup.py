342ff690098b5cc41f204b7f8ace66cf06ef532c
sax <s.apostolico@gmail.com>
2012-06-28 17:12:43 +0300

    0.1.4b1 fixed packaging

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ def scan_dir( target, packages=[], data_files=[] ):
             data_files.append([dirpath, [os.path.join(dirpath, f) for f in filenames]])
     return packages, data_files
 
-packages, data_files = scan_dir('maintenance')
+packages, data_files = scan_dir('webcam')
 
 if VERSION[3] == 'final':
     download_tag = RELEASE
