c4f05193e6789a1ab1aba7d71ecccf595af013d4
sax <s.apostolico@gmail.com>
2012-09-21 08:23:58 +0200

    updated sphinx conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -13,7 +13,7 @@
 
 import sys, os
 
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), os.pardir, os.pardir)))
 import webcam as app
 from django.conf import settings
 
